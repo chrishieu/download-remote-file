6cb35456a8a82656232108be81ba89d671353847
Hieu Synova Dev <hieu.bui@synova-solutions.com>
2018-05-15 12:54:09 +0700

    Initial commit

--- seo-beginner-auto-post.php
+++ seo-beginner-auto-post.php
@@ -1,6 +1,4 @@
 <?php
-define( 'BI_VERSION', '2.0' );
-
 define( 'BI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'BI_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BI_DELETE_LIMIT', 100000 );
